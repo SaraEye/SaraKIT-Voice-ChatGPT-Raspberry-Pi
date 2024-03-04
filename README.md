4703409b06fa239e8b110489c1f3e12abf90279d
Artur Majtczak <artur@majtczak.com>
2024-03-04 13:51:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# SaraKIT with Voice ChatGPT
+
+SaraKIT is equipped with three microphones and a specialized sound processor that cleanses the voice and supports speech recognition on Raspberry Pi from distances up to 16.4 feet (5 meters). Building a voice-activated ChatGPT can be achieved in various ways, and while there are numerous examples on GitHub, this guide proposes a solution based on the offline speech recognition tool Vosk for wake word detection and command recognition. For speech generation, we utilize Piper, an offline text-to-speech program. Both of these tools currently represent the best offline solutions for Raspberry Pi concerning Text to Speech and Speech to Text functionality. The offline aspect means constant internet connectivity is not required, ensuring privacy and security within your home environment, and importantly, it's a free solution.
+
+For more details on Piper, see here: [https://github.com/SaraEye/SaraKIT-Text-To-Speech-Piper-Raspberry-Pi](https://github.com/SaraEye/SaraKIT-Text-To-Speech-Piper-Raspberry-Pi)
+For more on Vosk, check out: [https://github.com/SaraEye/SaraKIT-Speech-Recognition-Vosk-Raspberry-Pi](https://github.com/SaraEye/SaraKIT-Speech-Recognition-Vosk-Raspberry-Pi)
+
+## Installation on SaraKIT
+
+Assuming the basic SaraKIT drivers are already installed ([Getting Started with SaraKIT](https://sarakit.saraai.com/getting-started/software)), follow these steps to install the required tools:
+
+```bash
+sudo apt-get update
+sudo apt-get install -y python3-pip python3-pyaudio libasound2-dev libfmt-dev libspdlog-dev
+
+sudo pip3 install vosk piper-tts openai
+
+git clone https://github.com/SaraEye/SaraKIT-Voice-ChatGPT-Raspberry-Pi VoiceChatGPT
+cd VoiceChatGPT
+```
+
+Before running, you need to enter your OpenAI API key, which you can obtain by registering on the OpenAI website. Insert your API key in the line:
+
+```python
+client = OpenAI(api_key="YOUR_API_KEY_HERE")
+```
+
+Set your wake word in the line:
+
+```python
+WakeWord="sarah"
+```
+
+If you wish to change the language from English or adjust the voice for Piper or Vosk, download and load the appropriate models. See the descriptions on [Piper's GitHub](https://github.com/SaraEye/SaraKIT-Text-To-Speech-Piper-Raspberry-Pi) and [Vosk's GitHub](https://github.com/SaraEye/SaraKIT-Speech-Recognition-Vosk-Raspberry-Pi) for guidance.
+
+To run:
+
+```bash
+python VoiceChatGPT.py
+```
+
+Initially, the chat waits for the wake word, by default "sarah". After recognizing it, you can ask ChatGPT anything, and it will respond verbally.
+
+This setup offers a potent way to integrate advanced voice interaction into your Raspberry Pi projects, leveraging the capabilities of SaraKIT and the intelligence of ChatGPT without the need for an always-on internet connection.
+
+
+Replace `YOUR_API_KEY_HERE` with your actual OpenAI API key. This README provides a comprehensive guide to setting up voice-activated ChatGPT with SaraKIT, emphasizing the project's offline capabilities and privacy advantages.
---
+++ VoiceChatGPT.py
@@ -0,0 +1,125 @@
+# sudo apt-get install pip
+# sudo apt-get install -y python3-pyaudio
+# sudo pip3 install vosk
+# ----pip install pyaudio
+
+import os
+import sys
+import json
+import time
+import contextlib
+import pyaudio
+import io
+from vosk import Model, KaldiRecognizer
+from openai import OpenAI
+from piper import PiperVoice
+import numpy as np
+
+WakeWord="sarah"
+#=============================================================================
+
+
+#Enter your api key
+#=============================================================================
+client = OpenAI(api_key="YOUR_API_KEY_HERE")
+
+
+#Select the language and voice (Text To Speech) for ChatGPT
+#=============================================================================
+#voice = PiperVoice.load("models/piper/pl/gosia/pl_PL-gosia-medium.onnx", "models/piper/pl/gosia/pl_PL-gosia-medium.onnx.json")
+voice = PiperVoice.load("models/piper/en/amy/en_US-amy-medium.onnx", "models/piper/en/amy/en_US-amy-medium.onnx.json")
+
+
+#Select the language and model for Speech Recognition
+#=============================================================================
+#model_path = "models/vosk-model-small-pl-0.22/"
+model_path = "models/vosk-model-small-en-us-0.15/"
+if not os.path.exists(model_path):
+    print(f"Model '{model_path}' was not found. Please check the path.")
+    exit(1)
+model = Model(model_path)
+
+
+
+# Initialization of PyAudio and speech recognition
+p = pyaudio.PyAudio()
+chunk_size=8192
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=chunk_size)
+recognizer = KaldiRecognizer(model, 16000)
+
+
+
+def text_to_speech(voice, text):
+    audio_stream = voice.synthesize_stream_raw(text)
+    pTTS = pyaudio.PyAudio()
+    stream = pTTS.open(format=pTTS.get_format_from_width(width=2),  # Assuming the audio is 16-bit
+                    channels=1,
+                    rate=22050,
+                    output=True,
+                    output_device_index=1)
+                    
+    # Calculate the number of silence samples to prepend
+    silence_duration=0.6
+    silence_samples = int(silence_duration * 22050)
+    silence_data = (np.zeros(silence_samples, dtype=np.int16)).tobytes()
+    # Play the silence
+    stream.write(silence_data)                    
+
+    # Play the stream chunk by chunk
+    for audio_bytes in audio_stream:
+        stream.write(audio_bytes)
+
+    stream.stop_stream()
+    stream.close()
+    pTTS.terminate()
+
+
+
+def askChatGPT(pytanie):
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": pytanie},
+        ],
+    )  
+    # Wyświetlanie odpowiedzi
+    resp=completion.choices[0].message.content
+    print(resp)
+    text_to_speech(voice, resp)
+
+
+
+os.system('clear')
+print("\nWaiting for WakeWord...")
+
+isChatActive=False
+last_sound_time = time.time()
+while True:
+    data = stream.read(chunk_size)
+    if recognizer.AcceptWaveform(data):
+        result_json = json.loads(recognizer.Result())
+        text = result_json.get('text', '')
+        if text:
+            print("\r" + text, end='\n')
+            last_sound_time = time.time()
+
+            if isChatActive:
+                stream.stop_stream()
+                askChatGPT(text)
+                print("\n")
+                stream.start_stream()
+                last_sound_time = time.time()
+                continue
+
+            if partial.lower()==WakeWord:
+                print("\nSpeak now...")
+                isChatActive=True                
+    else:
+        partial_json = json.loads(recognizer.PartialResult())
+        partial = partial_json.get('partial', '')
+        sys.stdout.write('\r' + partial)
+        sys.stdout.flush()
+
+    if time.time() - last_sound_time > 4 and isChatActive:
+        isChatActive = False
+        print("\nWaiting for WakeWord...")
